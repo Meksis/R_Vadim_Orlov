83f7c0cca32cdb904bdb343b493c967a94fc132b
Meksis <orlov.work.gen@gmail.com>
2023-11-09 19:31:43 +0300

    Update lesson 7.r

--- lesson 7/lesson 7.r
+++ lesson 7/lesson 7.r
@@ -1,59 +1,38 @@
+df <- read.csv("c:/Users/pasha/OneDrive/Рабочий стол/R/dz7/ДЗ2_vgsales.csv")
 library(shiny)
 library(dplyr)
 library(ggplot2)
-library(DT)
+print(head(df))
 
-data <- read.csv('df_hw7.csv')
+df %>%
+group_by(NA_Sales, EU_Sales, JP_Sales, Other_Sales) %>%
+summarise(mean = mean(NA_Sales), .groups = 'drop_last')
 
-ui <- fluidPage(
-  fluidRow(
-    column(3, textOutput("kpi_na")),
-    column(3, textOutput("kpi_eu")),
-    column(3, textOutput("kpi_jp")),
-    column(3, textOutput("kpi_other"))
-  ),
-  plotOutput("line_chart"),
-  plotOutput("bar_chart"),
-  DTOutput("data_table"),
-  selectInput("platform_filter", "Выберите платформу", choices = unique(data$Platform)),
-  sliderInput("year_filter", "Выберите диапазон годов", min(data$Year), max(data$Year), value = c(min(data$Year), max(data$Year)))
-)
+linear_plot(df, 'Year', c('NA_Sales', 'EU_Sales', 'JP_Sales', 'Other_Sales'))
 
-server <- function(input, output) {
-  kpi_na <- mean(data$NA_Sales, na.rm = TRUE)
-  kpi_eu <- mean(data$EU_Sales, na.rm = TRUE)
-  kpi_jp <- mean(data$JP_Sales, na.rm = TRUE)
-  kpi_other <- mean(data$Other_Sales, na.rm = TRUE)
-  output$kpi_na <- renderText(kpi_na)
-  output$kpi_eu <- renderText(kpi_eu)
-  output$kpi_jp <- renderText(kpi_jp)
-  output$kpi_other <- renderText(kpi_other)
-  output$line_chart <- renderPlot({
-    ggplot(data, aes(x = Year)) +
-      geom_line(aes(y = NA_Sales, color = "NA Sales")) +
-      geom_line(aes(y = EU_Sales, color = "EU Sales")) +
-      geom_line(aes(y = JP_Sales, color = "JP Sales")) +
-      geom_line(aes(y = Other_Sales, color = "Other Sales")) +
-      labs(title = "Динамика продаж", y = "Продажи", x = "Год")
-  })
-  output$bar_chart <- renderPlot({
-    data_genre <- data %>% group_by(Genre) %>% summarise(
-      NA_Sales = sum(NA_Sales, na.rm = TRUE),
-      EU_Sales = sum(EU_Sales, na.rm = TRUE),
-      JP_Sales = sum(JP_Sales, na.rm = TRUE),
-      Other_Sales = sum(Other_Sales, na.rm = TRUE)
-    )
-    ggplot(data_genre, aes(x = Genre)) +
-      geom_bar(aes(y = NA_Sales, fill = "NA Sales"), stat = "identity") +
-      geom_bar(aes(y = EU_Sales, fill = "EU Sales"), stat = "identity") +
-      geom_bar(aes(y = JP_Sales, fill = "JP Sales"), stat = "identity") +
-      geom_bar(aes(y = Other_Sales, fill = "Other Sales"), stat = "identity") +
-      labs(title = "Продажи по жанрам", y = "Продажи", x = "Жанр") +
-      theme(axis.text.x = element_text(angle = 45, hjust = 1))
-  })
-  output$data_table <- renderDT({
-    datatable(data)
-  })
-}
 
-shinyApp(ui, server)
+linear_plot(df, 'Year', c('NA_Sales', 'EU_Sales', 'JP_Sales', 'Other_Sales'))
+bar_plot(df, 'Genre', 'NA_Sales')
+bar_plot(df, 'Genre', 'EU_Sales')
+bar_plot(df, 'Genre', 'JP_Sales')
+bar_plot(df, 'Genre', 'Other_Sales')
+
+observe({
+    req(input$platform)
+    filtered_df <- df %>% filter(Platform %in% input$platform)
+    output$table <- renderTable({
+        filtered_df
+    })
+})
+
+observeEvent(input$yearrange, {
+    min_year <- input$yearrange[1]
+    max_year <- input$yearrange[2]
+    filtered_data <- reactive({
+        df %>%
+        filter(Year >= min_year & Year <= max_year)
+    })
+    output$table <- renderTable({
+        filtered_data()
+    })
+})
